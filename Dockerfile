2925af8a19cb15e20f1216c0db26a76ccd3930d0
Graeme Gregory <graeme@xora.org.uk>
2024-01-19 20:35:01 +0000

    Dockerfile : make us go to $HOME by default
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- Dockerfile
+++ Dockerfile
@@ -194,7 +194,7 @@ RUN <<EOF
     rm -rf qltools
 EOF
 
-WORKDIR /home/user/
+WORKDIR /
 
 COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 RUN chmod +x /usr/local/bin/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,5 @@ USER_ID=${LOCAL_USER_ID:-9001}
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
 export HOME=/home/user
-
+cd $HOME
 exec /usr/local/bin/gosu user "$@"
